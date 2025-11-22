554617850b13b3f68e23f445bb1480da5296c19d
gidratus <gidratus@gmail.com>
2025-11-22 23:24:03 +0300

    first

---
+++ WpfApp9/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Data.SqlClient;
+using System.Windows.Shapes;
+
+namespace WpfApp9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //await LoadCategoriesAsync();
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OpenSecondWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+    }
+}
